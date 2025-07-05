f05c680510658aeab6d8abeb4822cd68fabaafde
YC酱luyancib <luyancib@qq.com>
2025-07-05 22:22:58 +0800

    ci:make it fast for telegram bot

---
+++ scripts/rsubot.sh
@@ -0,0 +1,20 @@
+#!/bin/env bash
+
+msg="*$TITLE*
+\\#ci\\_$VERSION
+\\Branch\\: $BRANCH
+\`\`\`
+$COMMIT_MESSAGE
+\`\`\`
+[Commit]($COMMIT_URL)
+[Workflow run]($RUN_URL)
+"
+
+file="$1"
+
+curl -s -F document=@$file "https://api.telegram.org/bot$BOT_TOKEN/sendDocument" \
+	-F chat_id="$CHAT_ID" \
+	-F message_thread_id="$MESSAGE_THREAD_ID" \
+	-F "disable_web_page_preview=true" \
+	-F "parse_mode=markdownv2" \
+	-F caption="$msg"
