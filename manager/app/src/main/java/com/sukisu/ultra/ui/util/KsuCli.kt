3d3aacede56858fdea67133431e3002db7eba57e
Wang Han <416810799@qq.com>
2026-05-02 01:41:23 +0800

    manager: Close temp shell after module action (#3438)

--- manager/app/src/main/java/com/sukisu/ultra/ui/util/KsuCli.kt
+++ manager/app/src/main/java/com/sukisu/ultra/ui/util/KsuCli.kt
@@ -222,8 +222,6 @@ fun flashModule(
 fun runModuleAction(
     moduleId: String, onStdout: (String) -> Unit, onStderr: (String) -> Unit
 ): Boolean {
-    val shell = createRootShell(true)
-
     val stdoutCallback: CallbackList<String?> = object : CallbackList<String?>() {
         override fun onAddElement(s: String?) {
             onStdout(s ?: "")
@@ -236,8 +234,11 @@ fun runModuleAction(
         }
     }
 
-    val result = shell.newJob().add("${getKsuDaemonPath()} module action $moduleId")
-        .to(stdoutCallback, stderrCallback).exec()
+    val result = withNewRootShell(true) {
+        newJob().add("${getKsuDaemonPath()} module action $moduleId")
+            .to(stdoutCallback, stderrCallback).exec()
+    }
+
     Log.i("KernelSU", "Module runAction result: $result")
 
     return result.isSuccess
