tiann/KernelSU

manager: flicker-free scroll-to-top and channel-based UI events
- Add ScrollToTopOnChange helper: flicker-free scroll-to-top on
  sort/filter/search/pull-refresh for SuperUser, Module, ModuleRepo and
  Sulog (Material + Miuix); module toggle and nav-return no longer
  scroll or re-sort.
- Add pull-to-refresh to ModuleRepo (Material) for parity with Miuix.
- Replace the nullable effect with a buffered Channel + ObserveAsEvents;
  the reboot snackbar replaces the previous one (ModuleMiuix now uses a
  snackbar instead of a toast).

manager/app/src/main/java/me/weishu/kernelsu/ui/component/ScrollToTop.kt
@@ -1,0 +1,90 @@
+package me.weishu.kernelsu.ui.component
+
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.runtime.withFrameNanos
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeoutOrNull
+import kotlin.time.Duration.Companion.milliseconds
+
+private const val LIST_QUIET_MILLIS = 250L
+private const val LIST_SETTLE_TIMEOUT_MS = 3000L
+
+private class KeysRef {
+    var value: List<Any?>? = null
+}
+
+/**
+ * Flicker-free scroll-to-top when [keys] (sort/filter/search/refresh token) change while the
+ * composition is alive. Never scrolls on first composition or nav-return (NavDisplay recreates the
+ * screen and resets `remember`), so returning from a detail won't auto-scroll. For actions that
+ * don't change sort/filter (e.g. pull-to-refresh) pass a token that only increments then.
+ *
+ * [isBusy]: pass `{ isRefreshing }` for slow reloads so pinning lasts until the reload finishes;
+ * read it via rememberUpdatedState (uiState is a plain value param).
+ */
+@Composable
+fun ScrollToTopOnChange(
+    listState: LazyListState,
+    vararg keys: Any?,
+    onScrolledToTop: () -> Unit = {},
+    isBusy: () -> Boolean = { false },
+    observedList: () -> Any?,
+) {
+    val keysList = keys.asList()
+    val sideRef = remember { KeysRef() }
+    SideEffect {
+        val prev = sideRef.value
+        if (prev != null && prev != keysList) {
+            listState.requestScrollToItem(0)
+        }
+        sideRef.value = keysList
+    }
+    val effectRef = remember { KeysRef() }
+    LaunchedEffect(*keys) {
+        val prev = effectRef.value
+        effectRef.value = keysList
+        if (prev != null && prev != keysList) {
+            scrollToTopAfterListSettles(listState, isBusy = isBusy, observedList = observedList)
+            onScrolledToTop()
+        }
+    }
+}
+
+/**
+ * Pins the list to the top each frame until [observedList] is quiet for [LIST_QUIET_MILLIS] and
+ * [isBusy]() is false (capped by [LIST_SETTLE_TIMEOUT_MS]), overriding LazyColumn's key-based
+ * position restore. [observedList] must read a snapshot-trackable State (bridge uiState via
+ * rememberUpdatedState), otherwise settle detection falls back to a fixed timeout.
+ */
+suspend fun scrollToTopAfterListSettles(
+    listState: LazyListState,
+    isBusy: () -> Boolean = { false },
+    observedList: () -> Any?,
+): Unit = coroutineScope {
+    var settled = false
+    val watcher = launch {
+        while (true) {
+            val changed = withTimeoutOrNull(LIST_QUIET_MILLIS.milliseconds) {
+                snapshotFlow(observedList).drop(1).first()
+            }
+            if (changed == null && !isBusy()) break
+        }
+        settled = true
+    }
+    withTimeoutOrNull(LIST_SETTLE_TIMEOUT_MS.milliseconds) {
+        while (!settled) {
+            listState.requestScrollToItem(0)
+            withFrameNanos { }
+        }
+    }
+    watcher.cancel()
+    listState.requestScrollToItem(0)
+}

manager/app/src/main/java/me/weishu/kernelsu/ui/screen/modulerepo/ModuleRepoMiuix.kt
@@ -49,6 +49,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -76,6 +77,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import me.weishu.kernelsu.R
 import me.weishu.kernelsu.ui.component.ListPopupDefaults
+import me.weishu.kernelsu.ui.component.ScrollToTopOnChange
 import me.weishu.kernelsu.ui.component.SearchStatus
 import me.weishu.kernelsu.ui.component.dialog.ConfirmDialogHandle
 import me.weishu.kernelsu.ui.component.dialog.rememberConfirmDialog
@@ -384,9 +386,15 @@ fun ModuleRepoScreenMiuix(
             if (!isLoading && contentReady) {
                 val pullToRefreshState = rememberPullToRefreshState()
                 val lazyListState = rememberLazyListState()
-                LaunchedEffect(state.sortOrder) {
-                    lazyListState.scrollToItem(0)
-                }
+                val refreshTick = remember { mutableStateOf(0) }
+                val latestModules = rememberUpdatedState(state.modules)
+                val latestRefreshing = rememberUpdatedState(state.isRefreshing)
+                ScrollToTopOnChange(
+                    lazyListState,
+                    state.sortOrder,
+                    refreshTick.value,
+                    isBusy = { latestRefreshing.value },
+                ) { latestModules.value }
                 val refreshTexts = listOf(
                     stringResource(R.string.refresh_pulling),
                     stringResource(R.string.refresh_release),
@@ -396,7 +404,10 @@ fun ModuleRepoScreenMiuix(
                 PullToRefresh(
                     isRefreshing = state.isRefreshing,
                     pullToRefreshState = pullToRefreshState,
-                    onRefresh = actions.onRefresh,
+                    onRefresh = {
+                        actions.onRefresh()
+                        refreshTick.value++
+                    },
                     refreshTexts = refreshTexts,
                     contentPadding = PaddingValues(
                         top = innerPadding.calculateTopPadding() + 6.dp,

manager/app/src/main/java/me/weishu/kernelsu/ui/screen/sulog/SulogMiuix.kt
@@ -30,6 +30,7 @@ import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
@@ -39,6 +40,7 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -61,6 +63,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import me.weishu.kernelsu.R
 import me.weishu.kernelsu.ui.component.ListPopupDefaults
+import me.weishu.kernelsu.ui.component.ScrollToTopOnChange
 import me.weishu.kernelsu.ui.component.SearchStatus
 import me.weishu.kernelsu.ui.component.miuix.SearchBarFake
 import me.weishu.kernelsu.ui.component.miuix.SearchBox
@@ -115,6 +118,7 @@ fun SulogScreenMiuix(
     val blurActive = backdrop != null
     val barColor = if (blurActive) Color.Transparent else colorScheme.surface
     val pullToRefreshState = rememberPullToRefreshState()
+    val listState = rememberLazyListState()
     val fileSelector = buildSulogFileSelector(state.files, state.selectedFilePath)
     val refreshTexts = listOf(
         stringResource(R.string.refresh_pulling),
@@ -287,8 +291,16 @@ fun SulogScreenMiuix(
                     end = innerPadding.calculateEndPadding(layoutDirection),
                 ),
             ) {
+                val latestVisibleEntries = rememberUpdatedState(state.visibleEntries)
+                ScrollToTopOnChange(
+                    listState,
+                    state.searchText,
+                    state.selectedFilters,
+                    state.selectedFilePath,
+                ) { latestVisibleEntries.value }
                 Box(modifier = if (backdrop != null) Modifier.layerBackdrop(backdrop) else Modifier) {
                     LazyColumn(
+                        state = listState,
                         modifier = Modifier
                             .fillMaxHeight()
                             .scrollEndHaptic()

manager/app/src/main/java/me/weishu/kernelsu/ui/screen/superuser/SuperUserMaterial.kt
@@ -48,7 +48,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -59,10 +59,10 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
 import me.weishu.kernelsu.R
 import me.weishu.kernelsu.data.model.AppInfo
 import me.weishu.kernelsu.ui.component.AppIconImage
+import me.weishu.kernelsu.ui.component.ScrollToTopOnChange
 import me.weishu.kernelsu.ui.component.material.SearchAppBar
 import me.weishu.kernelsu.ui.component.material.SegmentedColumn
 import me.weishu.kernelsu.ui.component.material.SegmentedItem
@@ -76,18 +76,15 @@ fun SuperUserPagerMaterial(
     actions: SuperUserActions,
     bottomInnerPadding: Dp,
 ) {
-    val scope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
     val listState = rememberLazyListState()
     val searchListState = rememberLazyListState()
+    val refreshTick = remember { mutableStateOf(0) }
     val pullToRefreshState = rememberPullToRefreshState()
 
     var localSearchText by remember { mutableStateOf(uiState.searchStatus.searchText) }
     LaunchedEffect(uiState.searchStatus.searchText) {
         localSearchText = uiState.searchStatus.searchText
-    }
-    LaunchedEffect(uiState.sortOption) {
-        listState.scrollToItem(0)
     }
 
     val haptic = LocalHapticFeedback.current
@@ -102,7 +99,6 @@ fun SuperUserPagerMaterial(
                 onSearchTextChange = {
                     localSearchText = it
                     actions.onSearchTextChange(it)
-                    scope.launch { listState.scrollToItem(0) }
                 },
                 onClearClick = {
                     localSearchText = ""
@@ -282,6 +278,7 @@ fun SuperUserPagerMaterial(
             onRefresh = {
                 haptic.performHapticFeedback(HapticFeedbackType.VirtualKey)
                 actions.onRefresh()
+                refreshTick.value++
             },
             state = pullToRefreshState,
             indicator = {
@@ -293,6 +290,17 @@ fun SuperUserPagerMaterial(
             },
         ) {
             val expandedSearchUids = remember { mutableStateOf(setOf<Int>()) }
+
+            val latestGroupedApps = rememberUpdatedState(uiState.groupedApps)
+            val latestRefreshing = rememberUpdatedState(uiState.isRefreshing)
+            ScrollToTopOnChange(
+                listState,
+                uiState.sortOption,
+                uiState.showSystemApps,
+                uiState.showOnlyPrimaryUserApps,
+                refreshTick.value,
+                isBusy = { latestRefreshing.value },
+            ) { latestGroupedApps.value }
 
             LazyColumn(
                 state = listState,

manager/app/src/main/java/me/weishu/kernelsu/ui/screen/superuser/SuperUserMiuix.kt
@@ -41,6 +41,7 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -64,6 +65,7 @@ import me.weishu.kernelsu.R
 import me.weishu.kernelsu.data.model.AppInfo
 import me.weishu.kernelsu.ui.component.AppIconImage
 import me.weishu.kernelsu.ui.component.ListPopupDefaults
+import me.weishu.kernelsu.ui.component.ScrollToTopOnChange
 import me.weishu.kernelsu.ui.component.SearchStatus
 import me.weishu.kernelsu.ui.component.miuix.SearchBarFake
 import me.weishu.kernelsu.ui.component.miuix.SearchBox
@@ -400,14 +402,17 @@ fun SuperUserPagerMiuix(
         val layoutDirection = LocalLayoutDirection.current
         searchStatus.SearchBox {
             val lazyListState = rememberLazyListState()
-            val prevRefreshing = remember { booleanArrayOf(false) }
-            if (prevRefreshing[0] && !uiState.isRefreshing) {
-                lazyListState.requestScrollToItem(0)
-            }
-            prevRefreshing[0] = uiState.isRefreshing
-            LaunchedEffect(uiState.sortOption) {
-                lazyListState.scrollToItem(0)
-            }
+            val refreshTick = remember { mutableStateOf(0) }
+            val latestGroupedApps = rememberUpdatedState(uiState.groupedApps)
+            val latestRefreshing = rememberUpdatedState(uiState.isRefreshing)
+            ScrollToTopOnChange(
+                lazyListState,
+                uiState.sortOption,
+                uiState.showSystemApps,
+                uiState.showOnlyPrimaryUserApps,
+                refreshTick.value,
+                isBusy = { latestRefreshing.value },
+            ) { latestGroupedApps.value }
             val pullToRefreshState = rememberPullToRefreshState()
             val refreshTexts = listOf(
                 stringResource(R.string.refresh_pulling),
@@ -435,7 +440,10 @@ fun SuperUserPagerMiuix(
                 PullToRefresh(
                     isRefreshing = uiState.isRefreshing,
                     pullToRefreshState = pullToRefreshState,
-                    onRefresh = actions.onRefresh,
+                    onRefresh = {
+                        actions.onRefresh()
+                        refreshTick.value++
+                    },
                     refreshTexts = refreshTexts,
                     contentPadding = PaddingValues(
                         top = innerPadding.calculateTopPadding() + 6.dp,

vc-teahouse/SukiSU-Ultra

manager/app/src/main/java/com/sukisu/ultra/ui/component/ScrollToTop.kt
@@ -1,0 +1,90 @@
+package com.sukisu.ultra.ui.component
+
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.runtime.withFrameNanos
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeoutOrNull
+import kotlin.time.Duration.Companion.milliseconds
+
+private const val LIST_QUIET_MILLIS = 250L
+private const val LIST_SETTLE_TIMEOUT_MS = 3000L
+
+private class KeysRef {
+    var value: List<Any?>? = null
+}
+
+/**
+ * Flicker-free scroll-to-top when [keys] (sort/filter/search/refresh token) change while the
+ * composition is alive. Never scrolls on first composition or nav-return (NavDisplay recreates the
+ * screen and resets `remember`), so returning from a detail won't auto-scroll. For actions that
+ * don't change sort/filter (e.g. pull-to-refresh) pass a token that only increments then.
+ *
+ * [isBusy]: pass `{ isRefreshing }` for slow reloads so pinning lasts until the reload finishes;
+ * read it via rememberUpdatedState (uiState is a plain value param).
+ */
+@Composable
+fun ScrollToTopOnChange(
+    listState: LazyListState,
+    vararg keys: Any?,
+    onScrolledToTop: () -> Unit = {},
+    isBusy: () -> Boolean = { false },
+    observedList: () -> Any?,
+) {
+    val keysList = keys.asList()
+    val sideRef = remember { KeysRef() }
+    SideEffect {
+        val prev = sideRef.value
+        if (prev != null && prev != keysList) {
+            listState.requestScrollToItem(0)
+        }
+        sideRef.value = keysList
+    }
+    val effectRef = remember { KeysRef() }
+    LaunchedEffect(*keys) {
+        val prev = effectRef.value
+        effectRef.value = keysList
+        if (prev != null && prev != keysList) {
+            scrollToTopAfterListSettles(listState, isBusy = isBusy, observedList = observedList)
+            onScrolledToTop()
+        }
+    }
+}
+
+/**
+ * Pins the list to the top each frame until [observedList] is quiet for [LIST_QUIET_MILLIS] and
+ * [isBusy]() is false (capped by [LIST_SETTLE_TIMEOUT_MS]), overriding LazyColumn's key-based
+ * position restore. [observedList] must read a snapshot-trackable State (bridge uiState via
+ * rememberUpdatedState), otherwise settle detection falls back to a fixed timeout.
+ */
+suspend fun scrollToTopAfterListSettles(
+    listState: LazyListState,
+    isBusy: () -> Boolean = { false },
+    observedList: () -> Any?,
+): Unit = coroutineScope {
+    var settled = false
+    val watcher = launch {
+        while (true) {
+            val changed = withTimeoutOrNull(LIST_QUIET_MILLIS.milliseconds) {
+                snapshotFlow(observedList).drop(1).first()
+            }
+            if (changed == null && !isBusy()) break
+        }
+        settled = true
+    }
+    withTimeoutOrNull(LIST_SETTLE_TIMEOUT_MS.milliseconds) {
+        while (!settled) {
+            listState.requestScrollToItem(0)
+            withFrameNanos { }
+        }
+    }
+    watcher.cancel()
+    listState.requestScrollToItem(0)
+}

manager/app/src/main/java/com/sukisu/ultra/ui/screen/modulerepo/ModuleRepoMiuix.kt
@@ -49,6 +49,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -76,6 +77,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import com.sukisu.ultra.R
 import com.sukisu.ultra.ui.component.ListPopupDefaults
+import com.sukisu.ultra.ui.component.ScrollToTopOnChange
 import com.sukisu.ultra.ui.component.SearchStatus
 import com.sukisu.ultra.ui.component.dialog.ConfirmDialogHandle
 import com.sukisu.ultra.ui.component.dialog.rememberConfirmDialog
@@ -384,9 +386,15 @@ fun ModuleRepoScreenMiuix(
             if (!isLoading && contentReady) {
                 val pullToRefreshState = rememberPullToRefreshState()
                 val lazyListState = rememberLazyListState()
-                LaunchedEffect(state.sortOrder) {
-                    lazyListState.scrollToItem(0)
-                }
+                val refreshTick = remember { mutableStateOf(0) }
+                val latestModules = rememberUpdatedState(state.modules)
+                val latestRefreshing = rememberUpdatedState(state.isRefreshing)
+                ScrollToTopOnChange(
+                    lazyListState,
+                    state.sortOrder,
+                    refreshTick.value,
+                    isBusy = { latestRefreshing.value },
+                ) { latestModules.value }
                 val refreshTexts = listOf(
                     stringResource(R.string.refresh_pulling),
                     stringResource(R.string.refresh_release),
@@ -396,7 +404,10 @@ fun ModuleRepoScreenMiuix(
                 PullToRefresh(
                     isRefreshing = state.isRefreshing,
                     pullToRefreshState = pullToRefreshState,
-                    onRefresh = actions.onRefresh,
+                    onRefresh = {
+                        actions.onRefresh()
+                        refreshTick.value++
+                    },
                     refreshTexts = refreshTexts,
                     contentPadding = PaddingValues(
                         top = innerPadding.calculateTopPadding() + 6.dp,

manager/app/src/main/java/com/sukisu/ultra/ui/screen/sulog/SulogMiuix.kt
@@ -30,6 +30,7 @@ import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
@@ -39,6 +40,7 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -61,6 +63,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sukisu.ultra.R
 import com.sukisu.ultra.ui.component.ListPopupDefaults
+import com.sukisu.ultra.ui.component.ScrollToTopOnChange
 import com.sukisu.ultra.ui.component.SearchStatus
 import com.sukisu.ultra.ui.component.miuix.SearchBarFake
 import com.sukisu.ultra.ui.component.miuix.SearchBox
@@ -115,6 +118,7 @@ fun SulogScreenMiuix(
     val blurActive = backdrop != null
     val barColor = if (blurActive) Color.Transparent else colorScheme.surface
     val pullToRefreshState = rememberPullToRefreshState()
+    val listState = rememberLazyListState()
     val fileSelector = buildSulogFileSelector(state.files, state.selectedFilePath)
     val refreshTexts = listOf(
         stringResource(R.string.refresh_pulling),
@@ -287,8 +291,16 @@ fun SulogScreenMiuix(
                     end = innerPadding.calculateEndPadding(layoutDirection),
                 ),
             ) {
+                val latestVisibleEntries = rememberUpdatedState(state.visibleEntries)
+                ScrollToTopOnChange(
+                    listState,
+                    state.searchText,
+                    state.selectedFilters,
+                    state.selectedFilePath,
+                ) { latestVisibleEntries.value }
                 Box(modifier = if (backdrop != null) Modifier.layerBackdrop(backdrop) else Modifier) {
                     LazyColumn(
+                        state = listState,
                         modifier = Modifier
                             .fillMaxHeight()
                             .scrollEndHaptic()

manager/app/src/main/java/com/sukisu/ultra/ui/screen/superuser/SuperUserMaterial.kt
@@ -48,7 +48,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -59,10 +59,10 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
 import com.sukisu.ultra.R
 import com.sukisu.ultra.data.model.AppInfo
 import com.sukisu.ultra.ui.component.AppIconImage
+import com.sukisu.ultra.ui.component.ScrollToTopOnChange
 import com.sukisu.ultra.ui.component.material.SearchAppBar
 import com.sukisu.ultra.ui.component.material.SegmentedColumn
 import com.sukisu.ultra.ui.component.material.SegmentedItem
@@ -76,18 +76,15 @@ fun SuperUserPagerMaterial(
     actions: SuperUserActions,
     bottomInnerPadding: Dp,
 ) {
-    val scope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
     val listState = rememberLazyListState()
     val searchListState = rememberLazyListState()
+    val refreshTick = remember { mutableStateOf(0) }
     val pullToRefreshState = rememberPullToRefreshState()
 
     var localSearchText by remember { mutableStateOf(uiState.searchStatus.searchText) }
     LaunchedEffect(uiState.searchStatus.searchText) {
         localSearchText = uiState.searchStatus.searchText
-    }
-    LaunchedEffect(uiState.sortOption) {
-        listState.scrollToItem(0)
     }
 
     val haptic = LocalHapticFeedback.current
@@ -102,7 +99,6 @@ fun SuperUserPagerMaterial(
                 onSearchTextChange = {
                     localSearchText = it
                     actions.onSearchTextChange(it)
-                    scope.launch { listState.scrollToItem(0) }
                 },
                 onClearClick = {
                     localSearchText = ""
@@ -282,6 +278,7 @@ fun SuperUserPagerMaterial(
             onRefresh = {
                 haptic.performHapticFeedback(HapticFeedbackType.VirtualKey)
                 actions.onRefresh()
+                refreshTick.value++
             },
             state = pullToRefreshState,
             indicator = {
@@ -293,6 +290,17 @@ fun SuperUserPagerMaterial(
             },
         ) {
             val expandedSearchUids = remember { mutableStateOf(setOf<Int>()) }
+
+            val latestGroupedApps = rememberUpdatedState(uiState.groupedApps)
+            val latestRefreshing = rememberUpdatedState(uiState.isRefreshing)
+            ScrollToTopOnChange(
+                listState,
+                uiState.sortOption,
+                uiState.showSystemApps,
+                uiState.showOnlyPrimaryUserApps,
+                refreshTick.value,
+                isBusy = { latestRefreshing.value },
+            ) { latestGroupedApps.value }
 
             LazyColumn(
                 state = listState,

manager/app/src/main/java/com/sukisu/ultra/ui/screen/superuser/SuperUserMiuix.kt
@@ -41,6 +41,7 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -64,6 +65,7 @@ import com.sukisu.ultra.R
 import com.sukisu.ultra.data.model.AppInfo
 import com.sukisu.ultra.ui.component.AppIconImage
 import com.sukisu.ultra.ui.component.ListPopupDefaults
+import com.sukisu.ultra.ui.component.ScrollToTopOnChange
 import com.sukisu.ultra.ui.component.SearchStatus
 import com.sukisu.ultra.ui.component.miuix.SearchBarFake
 import com.sukisu.ultra.ui.component.miuix.SearchBox
@@ -400,14 +402,17 @@ fun SuperUserPagerMiuix(
         val layoutDirection = LocalLayoutDirection.current
         searchStatus.SearchBox {
             val lazyListState = rememberLazyListState()
-            val prevRefreshing = remember { booleanArrayOf(false) }
-            if (prevRefreshing[0] && !uiState.isRefreshing) {
-                lazyListState.requestScrollToItem(0)
-            }
-            prevRefreshing[0] = uiState.isRefreshing
-            LaunchedEffect(uiState.sortOption) {
-                lazyListState.scrollToItem(0)
-            }
+            val refreshTick = remember { mutableStateOf(0) }
+            val latestGroupedApps = rememberUpdatedState(uiState.groupedApps)
+            val latestRefreshing = rememberUpdatedState(uiState.isRefreshing)
+            ScrollToTopOnChange(
+                lazyListState,
+                uiState.sortOption,
+                uiState.showSystemApps,
+                uiState.showOnlyPrimaryUserApps,
+                refreshTick.value,
+                isBusy = { latestRefreshing.value },
+            ) { latestGroupedApps.value }
             val pullToRefreshState = rememberPullToRefreshState()
             val refreshTexts = listOf(
                 stringResource(R.string.refresh_pulling),
@@ -435,7 +440,10 @@ fun SuperUserPagerMiuix(
                 PullToRefresh(
                     isRefreshing = uiState.isRefreshing,
                     pullToRefreshState = pullToRefreshState,
-                    onRefresh = actions.onRefresh,
+                    onRefresh = {
+                        actions.onRefresh()
+                        refreshTick.value++
+                    },
                     refreshTexts = refreshTexts,
                     contentPadding = PaddingValues(
                         top = innerPadding.calculateTopPadding() + 6.dp,