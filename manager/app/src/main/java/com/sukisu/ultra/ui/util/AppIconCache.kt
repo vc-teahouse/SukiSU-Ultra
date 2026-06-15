tiann/KernelSU

manager: optimize icon loading logic and some minor fixes (#3503)
- Fix missing snackbar bottom padding in safe mode due to hidden install
button
- Prevent navigating to SuperUser and Module pages when kernel update
warning is shown
- Fix missing links on the About page in Japanese and other translations
by parsing HTML with Html.fromHtml instead of Regex
- Fix app icon loading for cross-profile-group users by resolving the
main (non-badge) user instead of always using the current user
- Refactor device name resolution to per-manufacturer dispatch and add
ZTE/Nubia support
- Remove deprecated getUsers() reflection fallback in KsuService
- change version separator from / to -

---------

Signed-off-by: u9521 <63995396+u9521@users.noreply.github.com>

manager/app/src/main/java/me/weishu/kernelsu/ui/util/AppIconCache.kt
@@ -4,30 +4,38 @@ import android.content.Context
 import android.content.pm.ApplicationInfo
 import android.graphics.Bitmap
 import android.os.Process
+import android.os.UserManager
+import android.util.Log
 import android.util.LruCache
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.sync.Semaphore
 import kotlinx.coroutines.sync.withPermit
 import kotlinx.coroutines.withContext
+import me.weishu.kernelsu.ui.util.AppIconCache.getMainUserId
 import me.zhanghai.android.appiconloader.AppIconLoader
+import org.lsposed.hiddenapibypass.HiddenApiBypass
+import java.util.concurrent.ConcurrentHashMap
 
 /**
- * Remap the uid to the current user to avoid SecurityException
- * when loading icons for cross-user apps.
+ * Remap the uid to the non-profile user possible to avoid SecurityException
+ * when loading icons for cross-profileGroup apps.
  * See: https://github.com/zhanghai/AppIconLoader/issues/3
  */
-fun ApplicationInfo.withCurrentUserUid(): ApplicationInfo {
-    val myUserId = Process.myUid() / 100000
-    val appId = uid % 100000
-    val targetUid = myUserId * 100000 + appId
-    if (uid == targetUid) return this
+fun ApplicationInfo.withMainUserUid(context: Context): ApplicationInfo {
+    val mainUserId = getMainUserId(context)
+    val appId = this.uid % 100000
+    val targetUid = mainUserId * 100000 + appId
+
+    if (this.uid == targetUid) return this
     return ApplicationInfo(this).apply { uid = targetUid }
 }
 
 object AppIconCache {
+    private const val TAG = "AppIconCache"
     private val maxMemory = Runtime.getRuntime().maxMemory() / 1024
     private val cacheSize = (maxMemory / 8).toInt()
     private val loadSemaphore = Semaphore(4)
+    private val iconLoaders = ConcurrentHashMap<Int, AppIconLoader>()
 
     private val lruCache = object : LruCache<String, Bitmap>(cacheSize) {
         override fun sizeOf(key: String, value: Bitmap): Int {
@@ -35,44 +43,131 @@ object AppIconCache {
         }
     }
 
-    fun getFromCache(applicationInfo: ApplicationInfo): Bitmap? {
-        val key = buildKey(applicationInfo)
-        synchronized(lruCache) { return lruCache.get(key) }
+    @Volatile
+    private var otherProfileGroupUserIds = setOf<Int>()
+
+    private fun userId(uid: Int): Int = uid / 100000
+
+    private fun isOtherProfileGroupUser(uid: Int): Boolean {
+        return otherProfileGroupUserIds.contains(userId(uid))
     }
 
-    private fun buildKey(applicationInfo: ApplicationInfo): String {
-        return "${applicationInfo.packageName}:${applicationInfo.uid}:${applicationInfo.sourceDir}"
+    private fun markOtherProfileGroupUser(uid: Int) {
+        val id = userId(uid)
+        if (!otherProfileGroupUserIds.contains(id)) {
+            synchronized(this) {
+                otherProfileGroupUserIds = otherProfileGroupUserIds + id
+            }
+        }
+    }
+
+    @Volatile
+    private var cachedMainUserId: Int? = null
+
+    internal fun getMainUserId(context: Context): Int {
+        cachedMainUserId?.let { return it }
+
+        synchronized(this) {
+            cachedMainUserId?.let { return it }
+
+            val um = context.getSystemService(Context.USER_SERVICE) as UserManager
+            val profiles = um.userProfiles
+            var foundMainUserId = userId(Process.myUid())
+            try {
+                HiddenApiBypass.addHiddenApiExemptions("Landroid/os/UserManager;->hasBadge(I)Z")
+                val hasBadgeMethod = UserManager::class.java.getMethod("hasBadge", Int::class.javaPrimitiveType)
+                for (profile in profiles) {
+                    val id = profile.hashCode()
+                    val hasBadge = hasBadgeMethod.invoke(um, id) as Boolean
+                    if (!hasBadge) {
+                        foundMainUserId = id
+                        break
+                    }
+                }
+            } catch (e: Exception) {
+                Log.w(TAG, "Failed to resolve main user id via hasBadge", e)
+            }
+            cachedMainUserId = foundMainUserId
+            return foundMainUserId
+        }
+    }
+
+    private fun buildKey(applicationInfo: ApplicationInfo, size: Int): String {
+        return "${applicationInfo.packageName}:${applicationInfo.uid}:${applicationInfo.sourceDir}:${size}"
+    }
+
+    fun getCached(applicationInfo: ApplicationInfo, size: Int): Bitmap? {
+        val key = buildKey(applicationInfo, size)
+        return lruCache.get(key)?.takeIf { !it.isRecycled }
+    }
+
+    /**
+     * Load icon bitmap using AppIconLoader with cross-profile fallback.
+     * Called on IO thread, returns a software bitmap.
+     */
+    private fun loadIconBitmap(context: Context, applicationInfo: ApplicationInfo, size: Int): Bitmap {
+        val loader = iconLoaders.getOrPut(size) {
+            AppIconLoader(size, false, context.applicationContext)
+        }
+
+        return if (isOtherProfileGroupUser(applicationInfo.uid)) {
+            loader.loadIcon(applicationInfo.withMainUserUid(context))
+        } else {
+            try {
+                loader.loadIcon(applicationInfo)
+            } catch (_: SecurityException) {
+                Log.d(TAG, "SecurityException loading icon for userId ${userId(applicationInfo.uid)}, retrying with main user uid")
+                markOtherProfileGroupUser(applicationInfo.uid)
+                loader.loadIcon(applicationInfo.withMainUserUid(context))
+            }
+        }
+    }
+
+    /**
+     * Synchronous icon loading for non-UI callers (e.g. WebView shouldInterceptRequest).
+     * Returns a software bitmap suitable for compress/encode. Does NOT copy to HARDWARE config.
+     */
+    fun loadIconSync(context: Context, applicationInfo: ApplicationInfo, size: Int): Bitmap {
+        val key = buildKey(applicationInfo, size)
+
+        lruCache.get(key)?.let {
+            if (!it.isRecycled) return it
+            lruCache.remove(key)
+        }
+
+        val bitmap = loadIconBitmap(context, applicationInfo, size)
+        bitmap.prepareToDraw()
+        lruCache.put(key, bitmap)
+        return bitmap
     }
 
     suspend fun loadIcon(context: Context, applicationInfo: ApplicationInfo, size: Int): Bitmap {
-        val key = buildKey(applicationInfo)
+        val key = buildKey(applicationInfo, size)
 
-        synchronized(lruCache) {
-            val cached = lruCache.get(key)
-            if (cached != null) return cached
+        lruCache.get(key)?.let {
+            if (!it.isRecycled) return it
+            lruCache.remove(key)
         }
 
         return loadSemaphore.withPermit {
-            synchronized(lruCache) {
-                val cached = lruCache.get(key)
-                if (cached != null) return@withPermit cached
+            lruCache.get(key)?.let {
+                if (!it.isRecycled) return@withPermit it
+                lruCache.remove(key)
             }
 
             withContext(Dispatchers.IO) {
-                val loader = AppIconLoader(size, false, context)
-                val bitmap = loader.loadIcon(applicationInfo.withCurrentUserUid())
+                val bitmap = loadIconBitmap(context, applicationInfo, size)
 
                 val gpuBitmap = try {
                     bitmap.copy(Bitmap.Config.HARDWARE, false)?.also {
                         bitmap.recycle()
                     } ?: bitmap.also { it.prepareToDraw() }
-                } catch (_: Exception) {
+                } catch (e: Exception) {
+                    Log.d(TAG, "Failed to copy bitmap to HARDWARE config", e)
                     bitmap.also { it.prepareToDraw() }
                 }
 
-                synchronized(lruCache) {
-                    lruCache.put(key, gpuBitmap)
-                }
+                lruCache.put(key, gpuBitmap)
 
                 gpuBitmap
             }

manager/app/src/main/java/me/weishu/kernelsu/ui/webui/WebViewHelper.kt
@@ -21,7 +21,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import me.weishu.kernelsu.R
 import me.weishu.kernelsu.data.repository.ModuleRepositoryImpl
+import me.weishu.kernelsu.ui.util.AppIconCache
 import me.weishu.kernelsu.ui.util.createRootShell
+import me.weishu.kernelsu.ui.util.withMainUserUid
 import me.weishu.kernelsu.ui.viewmodel.SuperUserViewModel
 import java.io.File
 
@@ -107,14 +109,28 @@ internal suspend fun prepareWebView(
                     if (url.scheme.equals("ksu", ignoreCase = true) && url.host.equals("icon", ignoreCase = true)) {
                         val packageName = url.path?.substring(1)
                         if (!packageName.isNullOrEmpty()) {
-                            val icon = AppIconUtil.loadAppIconSync(activity, packageName, 512)
-                            if (icon != null) {
+                            val appInfo = SuperUserViewModel.apps
+                                .find { it.packageName == packageName }
+                                ?.packageInfo?.applicationInfo
+                            if (appInfo != null) {
+                                val icon = AppIconCache.loadIconSync(activity, appInfo.withMainUserUid(activity), 512)
                                 val stream = java.io.ByteArrayOutputStream()
                                 icon.compress(android.graphics.Bitmap.CompressFormat.PNG, 100, stream)
                                 return WebResourceResponse(
                                     "image/png", null, 200, "OK",
                                     mapOf("Access-Control-Allow-Origin" to "*"),
                                     java.io.ByteArrayInputStream(stream.toByteArray())
+                                )
+                            } else {
+                                val errorMsg = "No such package"
+                                val errorStream = java.io.ByteArrayInputStream(errorMsg.toByteArray(Charsets.UTF_8))
+                                return WebResourceResponse(
+                                    "text/plain",
+                                    "utf-8",
+                                    404,
+                                    "Not Found",
+                                    mapOf("Access-Control-Allow-Origin" to "*"),
+                                    errorStream
                                 )
                             }
                         }

vc-teahouse/SukiSU-Ultra

manager/app/src/main/java/com/sukisu/ultra/ui/util/AppIconCache.kt
@@ -4,30 +4,38 @@ import android.content.Context
 import android.content.pm.ApplicationInfo
 import android.graphics.Bitmap
 import android.os.Process
+import android.os.UserManager
+import android.util.Log
 import android.util.LruCache
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.sync.Semaphore
 import kotlinx.coroutines.sync.withPermit
 import kotlinx.coroutines.withContext
+import com.sukisu.ultra.ui.util.AppIconCache.getMainUserId
 import me.zhanghai.android.appiconloader.AppIconLoader
+import org.lsposed.hiddenapibypass.HiddenApiBypass
+import java.util.concurrent.ConcurrentHashMap
 
 /**
- * Remap the uid to the current user to avoid SecurityException
- * when loading icons for cross-user apps.
+ * Remap the uid to the non-profile user possible to avoid SecurityException
+ * when loading icons for cross-profileGroup apps.
  * See: https://github.com/zhanghai/AppIconLoader/issues/3
  */
-fun ApplicationInfo.withCurrentUserUid(): ApplicationInfo {
-    val myUserId = Process.myUid() / 100000
-    val appId = uid % 100000
-    val targetUid = myUserId * 100000 + appId
-    if (uid == targetUid) return this
+fun ApplicationInfo.withMainUserUid(context: Context): ApplicationInfo {
+    val mainUserId = getMainUserId(context)
+    val appId = this.uid % 100000
+    val targetUid = mainUserId * 100000 + appId
+
+    if (this.uid == targetUid) return this
     return ApplicationInfo(this).apply { uid = targetUid }
 }
 
 object AppIconCache {
+    private const val TAG = "AppIconCache"
     private val maxMemory = Runtime.getRuntime().maxMemory() / 1024
     private val cacheSize = (maxMemory / 8).toInt()
     private val loadSemaphore = Semaphore(4)
+    private val iconLoaders = ConcurrentHashMap<Int, AppIconLoader>()
 
     private val lruCache = object : LruCache<String, Bitmap>(cacheSize) {
         override fun sizeOf(key: String, value: Bitmap): Int {
@@ -35,44 +43,131 @@ object AppIconCache {
         }
     }
 
-    fun getFromCache(applicationInfo: ApplicationInfo): Bitmap? {
-        val key = buildKey(applicationInfo)
-        synchronized(lruCache) { return lruCache.get(key) }
+    @Volatile
+    private var otherProfileGroupUserIds = setOf<Int>()
+
+    private fun userId(uid: Int): Int = uid / 100000
+
+    private fun isOtherProfileGroupUser(uid: Int): Boolean {
+        return otherProfileGroupUserIds.contains(userId(uid))
     }
 
-    private fun buildKey(applicationInfo: ApplicationInfo): String {
-        return "${applicationInfo.packageName}:${applicationInfo.uid}:${applicationInfo.sourceDir}"
+    private fun markOtherProfileGroupUser(uid: Int) {
+        val id = userId(uid)
+        if (!otherProfileGroupUserIds.contains(id)) {
+            synchronized(this) {
+                otherProfileGroupUserIds = otherProfileGroupUserIds + id
+            }
+        }
+    }
+
+    @Volatile
+    private var cachedMainUserId: Int? = null
+
+    internal fun getMainUserId(context: Context): Int {
+        cachedMainUserId?.let { return it }
+
+        synchronized(this) {
+            cachedMainUserId?.let { return it }
+
+            val um = context.getSystemService(Context.USER_SERVICE) as UserManager
+            val profiles = um.userProfiles
+            var foundMainUserId = userId(Process.myUid())
+            try {
+                HiddenApiBypass.addHiddenApiExemptions("Landroid/os/UserManager;->hasBadge(I)Z")
+                val hasBadgeMethod = UserManager::class.java.getMethod("hasBadge", Int::class.javaPrimitiveType)
+                for (profile in profiles) {
+                    val id = profile.hashCode()
+                    val hasBadge = hasBadgeMethod.invoke(um, id) as Boolean
+                    if (!hasBadge) {
+                        foundMainUserId = id
+                        break
+                    }
+                }
+            } catch (e: Exception) {
+                Log.w(TAG, "Failed to resolve main user id via hasBadge", e)
+            }
+            cachedMainUserId = foundMainUserId
+            return foundMainUserId
+        }
+    }
+
+    private fun buildKey(applicationInfo: ApplicationInfo, size: Int): String {
+        return "${applicationInfo.packageName}:${applicationInfo.uid}:${applicationInfo.sourceDir}:${size}"
+    }
+
+    fun getCached(applicationInfo: ApplicationInfo, size: Int): Bitmap? {
+        val key = buildKey(applicationInfo, size)
+        return lruCache.get(key)?.takeIf { !it.isRecycled }
+    }
+
+    /**
+     * Load icon bitmap using AppIconLoader with cross-profile fallback.
+     * Called on IO thread, returns a software bitmap.
+     */
+    private fun loadIconBitmap(context: Context, applicationInfo: ApplicationInfo, size: Int): Bitmap {
+        val loader = iconLoaders.getOrPut(size) {
+            AppIconLoader(size, false, context.applicationContext)
+        }
+
+        return if (isOtherProfileGroupUser(applicationInfo.uid)) {
+            loader.loadIcon(applicationInfo.withMainUserUid(context))
+        } else {
+            try {
+                loader.loadIcon(applicationInfo)
+            } catch (_: SecurityException) {
+                Log.d(TAG, "SecurityException loading icon for userId ${userId(applicationInfo.uid)}, retrying with main user uid")
+                markOtherProfileGroupUser(applicationInfo.uid)
+                loader.loadIcon(applicationInfo.withMainUserUid(context))
+            }
+        }
+    }
+
+    /**
+     * Synchronous icon loading for non-UI callers (e.g. WebView shouldInterceptRequest).
+     * Returns a software bitmap suitable for compress/encode. Does NOT copy to HARDWARE config.
+     */
+    fun loadIconSync(context: Context, applicationInfo: ApplicationInfo, size: Int): Bitmap {
+        val key = buildKey(applicationInfo, size)
+
+        lruCache.get(key)?.let {
+            if (!it.isRecycled) return it
+            lruCache.remove(key)
+        }
+
+        val bitmap = loadIconBitmap(context, applicationInfo, size)
+        bitmap.prepareToDraw()
+        lruCache.put(key, bitmap)
+        return bitmap
     }
 
     suspend fun loadIcon(context: Context, applicationInfo: ApplicationInfo, size: Int): Bitmap {
-        val key = buildKey(applicationInfo)
+        val key = buildKey(applicationInfo, size)
 
-        synchronized(lruCache) {
-            val cached = lruCache.get(key)
-            if (cached != null) return cached
+        lruCache.get(key)?.let {
+            if (!it.isRecycled) return it
+            lruCache.remove(key)
         }
 
         return loadSemaphore.withPermit {
-            synchronized(lruCache) {
-                val cached = lruCache.get(key)
-                if (cached != null) return@withPermit cached
+            lruCache.get(key)?.let {
+                if (!it.isRecycled) return@withPermit it
+                lruCache.remove(key)
             }
 
             withContext(Dispatchers.IO) {
-                val loader = AppIconLoader(size, false, context)
-                val bitmap = loader.loadIcon(applicationInfo.withCurrentUserUid())
+                val bitmap = loadIconBitmap(context, applicationInfo, size)
 
                 val gpuBitmap = try {
                     bitmap.copy(Bitmap.Config.HARDWARE, false)?.also {
                         bitmap.recycle()
                     } ?: bitmap.also { it.prepareToDraw() }
-                } catch (_: Exception) {
+                } catch (e: Exception) {
+                    Log.d(TAG, "Failed to copy bitmap to HARDWARE config", e)
                     bitmap.also { it.prepareToDraw() }
                 }
 
-                synchronized(lruCache) {
-                    lruCache.put(key, gpuBitmap)
-                }
+                lruCache.put(key, gpuBitmap)
 
                 gpuBitmap
             }

manager/app/src/main/java/com/sukisu/ultra/ui/webui/WebViewHelper.kt
@@ -21,7 +21,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import com.sukisu.ultra.R
 import com.sukisu.ultra.data.repository.ModuleRepositoryImpl
+import com.sukisu.ultra.ui.util.AppIconCache
 import com.sukisu.ultra.ui.util.createRootShell
+import com.sukisu.ultra.ui.util.withMainUserUid
 import com.sukisu.ultra.ui.viewmodel.SuperUserViewModel
 import java.io.File
 
@@ -107,14 +109,28 @@ internal suspend fun prepareWebView(
                     if (url.scheme.equals("ksu", ignoreCase = true) && url.host.equals("icon", ignoreCase = true)) {
                         val packageName = url.path?.substring(1)
                         if (!packageName.isNullOrEmpty()) {
-                            val icon = AppIconUtil.loadAppIconSync(activity, packageName, 512)
-                            if (icon != null) {
+                            val appInfo = SuperUserViewModel.apps
+                                .find { it.packageName == packageName }
+                                ?.packageInfo?.applicationInfo
+                            if (appInfo != null) {
+                                val icon = AppIconCache.loadIconSync(activity, appInfo.withMainUserUid(activity), 512)
                                 val stream = java.io.ByteArrayOutputStream()
                                 icon.compress(android.graphics.Bitmap.CompressFormat.PNG, 100, stream)
                                 return WebResourceResponse(
                                     "image/png", null, 200, "OK",
                                     mapOf("Access-Control-Allow-Origin" to "*"),
                                     java.io.ByteArrayInputStream(stream.toByteArray())
+                                )
+                            } else {
+                                val errorMsg = "No such package"
+                                val errorStream = java.io.ByteArrayInputStream(errorMsg.toByteArray(Charsets.UTF_8))
+                                return WebResourceResponse(
+                                    "text/plain",
+                                    "utf-8",
+                                    404,
+                                    "Not Found",
+                                    mapOf("Access-Control-Allow-Origin" to "*"),
+                                    errorStream
                                 )
                             }
                         }