e1fdd39ab512deced4c8fb9d52cf1efddce640d0
5ec1cff <56485584+5ec1cff@users.noreply.github.com>
2026-06-17 14:45:53 +0800

    kernel, ksud: refactor sucompat (#3521)
    - Execute ksud by opening fd with ksu cred and redirecting to execveat,
    remove READ_SEARCH capability from effective caps.
    - Redirect newfstatat/faccessat's `su` path to `ksud` .
    - Now sucompat requires `/data/adb/ksud` to exist in caller's root, so
    chrooted process won't be affected unexpectedly.
    - Remove fallback to sh. User should install manager and open it after
    first installation to install ksud to enable sucompat after first
    installation.

--- kernel/core/init.c
+++ kernel/core/init.c
@@ -130,6 +130,7 @@ int __init kernelsu_init(void)
     ksu_cred = prepare_creds();
     if (!ksu_cred) {
         pr_err("prepare cred failed!\n");
+        return -ENOSYS;
     }
 
     ksu_init_symbol_resolver();
@@ -223,9 +224,7 @@ void __exit kernelsu_exit(void)
     ksu_sulog_exit();
     ksu_feature_exit();
 
-    if (ksu_cred) {
-        put_cred(ksu_cred);
-    }
+    put_cred(ksu_cred);
 }
 
 #if NEED_OWN_STACKPROTECTOR
